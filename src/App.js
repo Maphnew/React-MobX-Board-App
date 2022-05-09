f305be3b64b6ca4390088cb8ff58fd7ade039f97
김성찬 <sungchan.kim@datastreams.co.kr>
2022-05-09 15:40:02 +0900

    board app 완성2 todo: 상태관리라이브러리 적용

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import ArticleList from "./component/ArticleList";
 import Content from "./component/Content";
 
 function App() {
-    const [article, setArticle] = useState({});
+    const [article, setArticle] = useState({ number: "", title: "", author: "", date: "", contente: "" });
     return (
         <>
             <h2>Board App 📋</h2>
             <div className="App">
-                <ArticleList setArticle={setArticle} />
-                <Content article={article} />
+                <ArticleList article={article} setArticle={setArticle} />
+                <Content article={article} setArticle={setArticle} />
             </div>
         </>
     );
--- src/component/Content.js
+++ src/component/Content.js
@@ -1,15 +1,48 @@
 const Content = (props) => {
-    const { article } = props;
+    const { article, setArticle } = props;
+    const submitHandler = () => {
+        console.log("submit");
+    };
+    const contentChangeHandler = (e) => {
+        const tempArticle = Object.assign(article);
+        tempArticle[e.target.id] = e.target.value;
+        setArticle({ ...tempArticle });
+    };
     return (
         <section className="content">
-            <h3>Title: {article.title}</h3>
-            <div className="content__info">
-                <p>사용자: {article.author}</p>
-                <p>작성일: {article.date}</p>
-            </div>
-            <div className="content__text">
-                <textarea className="content__text__textarea" rows={10} value={article.content} />
-            </div>
+            <form onSubmit={submitHandler}>
+                <h3>Title</h3>
+                <input id="title" className="content__title" value={article.title} onChange={contentChangeHandler} />
+                <div className="content__info">
+                    <label>
+                        작성자
+                        <input
+                            id="author"
+                            className="content__info__user-input"
+                            value={article.author}
+                            onChange={contentChangeHandler}
+                        />
+                    </label>
+                    <label>
+                        작성일
+                        <input
+                            id="date"
+                            className="content__info__create-date"
+                            value={article.date}
+                            onChange={contentChangeHandler}
+                        />
+                    </label>
+                </div>
+                <div className="content__text">
+                    <textarea
+                        id="content"
+                        className="content__text__textarea"
+                        rows={10}
+                        value={article.content}
+                        onChange={contentChangeHandler}
+                    />
+                </div>
+            </form>
         </section>
     );
 };
